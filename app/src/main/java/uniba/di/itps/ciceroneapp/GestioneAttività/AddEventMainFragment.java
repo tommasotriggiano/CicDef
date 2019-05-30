4082b524ea8d1c1c9b32a1996895911e5c855103
Tom96 <Tom96@f2a922d8-b100-4939-ac2e-478eb0d22949>
2019-05-30 16:01:45 +0000

    file string

--- app/src/main/java/uniba/di/itps/ciceroneapp/GestioneAttività/AddEventMainFragment.java
+++ app/src/main/java/uniba/di/itps/ciceroneapp/GestioneAttività/AddEventMainFragment.java
@@ -1,6 +1,7 @@
 package uniba.di.itps.ciceroneapp.GestioneAttività;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +14,10 @@ public class AddEventMainFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basic_information, container, false);
+        return inflater.inflate(R.layout.fragment_add_event_main, container, false);
     }}
 
 
