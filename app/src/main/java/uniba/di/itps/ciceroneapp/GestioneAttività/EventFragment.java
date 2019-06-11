8af055cd058ea6fbab3bf47da67d4a7ee2bd6fca
Elio <ramses0173>
2019-06-11 13:00:48 +0200

    Create le tab

--- app/src/main/java/uniba/di/itps/ciceroneapp/GestioneAttività/EventFragment.java
+++ app/src/main/java/uniba/di/itps/ciceroneapp/GestioneAttività/EventFragment.java
@@ -3,108 +3,50 @@ package uniba.di.itps.ciceroneapp.GestioneAttività;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import uniba.di.itps.ciceroneapp.GestioneAttività.adapter.TabsEventAdapter;
+import uniba.di.itps.ciceroneapp.GestioneAttività.gestioneMyEvent.MyEventCreatedFragment;
 import uniba.di.itps.ciceroneapp.R;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link EventFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link EventFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class EventFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public EventFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EventFragment newInstance(String param1, String param2) {
-        EventFragment fragment = new EventFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_event,container,false);
+        ViewPager viewPager = view.findViewById(R.id.eventViewPage);
+        setupViewPager(viewPager);
+        TabLayout tabs = view.findViewById(R.id.eventTabLayout);
+        tabs.setupWithViewPager(viewPager);
+        return view;
+
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event, container, false);
+
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        TabsEventAdapter adapter = new TabsEventAdapter(getChildFragmentManager());
+        adapter.addFragment(new MyEventCreatedFragment(), getContext().getResources().getString(R.string.createdTab));
+        adapter.addFragment(new TabRequestedFragment(), getContext().getResources().getString(R.string.requestedTab));
+        viewPager.setAdapter(adapter);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+
+        @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/uniba/di/itps/ciceroneapp/GestioneAttività/TabRequestedFragment.java
+++ app/src/main/java/uniba/di/itps/ciceroneapp/GestioneAttività/TabRequestedFragment.java
@@ -1,4 +1,4 @@
-package uniba.di.itps.ciceroneapp;
+package uniba.di.itps.ciceroneapp.GestioneAttività;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import uniba.di.itps.ciceroneapp.R;
 
 
 /**
